36777230d4433ee2de7bf91a9ade0d1753b4f6c8
Anmel01 <bettoanamel@gmail.com>
2024-11-19 13:39:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual a montanha mais alta do mundo?',
+    'A montanha mais alta do mundo é o Monte Everest'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos ossos temos no corpo?',
+    'Temos 206 ossos no corpo humano'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o nome popular do cloreto de sódio?',
+    'O nome popular do cloreto de sódio é Sal de cozinha'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual animal põe o maior ovo?',
+    'O animal que põe omaior ovo é o avestruz'
+)
